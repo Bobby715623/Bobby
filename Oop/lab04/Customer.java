0c25ba97bd26a667e0180fa6ff77fdc7c44ec20d
Bobby715623 <109953502+Bobby715623@users.noreply.github.com>
2023-02-24 01:29:07 +0700

    Update Customer.java

--- Oop/lab04/Customer.java
+++ Oop/lab04/Customer.java
@@ -1,27 +1,6 @@
 
-public class Account {
-    public double balance;
+public class Customer {
     public String name;
-    
-    public void deposit(double b){
-        if (b >= 0){
-        balance = balance + b;
-    }else{
-                System.out.println("The balance variable must be greater than or equal to zero.");}
-    }
-    public double withdraw(double b){
-        if (balance < b){
-            System.out.println("Your account balance is insufficient.");
-            return balance;
-        }if (b >= 0){
-            balance = balance - b;
-            return balance;
-        }else{
-            System.out.println("The balance variable must be greater than or equal to zero");
-            return b = 0;
-        }
-    }
-    public void showInfo(){
-        System.out.println("In "+name+" account, there is a balance equal to "+balance+" baht.");
-    }
+    public MyDate DOB;
+    public Account acct;
 }
